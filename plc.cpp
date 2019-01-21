2ef48e4b2cac536777be3772c1090b12dfd0300c
Jordan (Jordan) Kolody <kolj4600@36batman.cs.uleth.ca>
2019-01-20 17:05:08 -0700

    PLC compiles

--- plc.cpp
+++ plc.cpp
@@ -12,7 +12,8 @@ int main ()
    // variables for file names
    
    /*remove testLang.txt when project complete*/
-   string CodeFileName = "testLang.txt", TokenFileName;
+   string CodeFileName = "testLang.txt";
+   //TokenFileName;
 
    //get the file name for input and then declare and open file
    cout<<"Welcome to compiler4600, please enter your code input file name: ";
@@ -20,7 +21,8 @@ int main ()
    /*fix the commented out cin when complete */
    //cin>>CodeFileName;
    
-   ifstream TokenFileIn, CodeFileIn.open(CodeFileName);
+   ifstream CodeFileIn;// TokenFileIn, 
+   CodeFileIn.open(CodeFileName.c_str());
 
    //get the file name for output and then declare and open file
    cout<<endl<< "Now, please enter your scanner output file: ";
@@ -55,6 +57,9 @@ int main ()
      cout<< "Scan Complete" << endl;
      else
      cout << "Scan errors have occoured" <<endl;
-    */
+   */
+
+
+   CodeFileIn.close();
    return 0;
 }
