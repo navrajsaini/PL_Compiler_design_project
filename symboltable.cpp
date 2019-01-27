e547c9af02b920e2d9052609860a38e004771cc2
Navraj Saini <saini4@36superman.cs.uleth.ca>
2019-01-27 14:20:00 -0700

    fixed the extra increment

--- symboltable.cpp
+++ symboltable.cpp
@@ -42,119 +42,119 @@ int Symtable::insert(string s)
 	 Token begin(Symbol::begin, hashval, s);
 	 htable.insert (htable.begin()+hashval, begin);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "end")
       {
 	 Token end(Symbol::end, hashval, s);
 	 htable.insert (htable.begin()+hashval, end);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "const")
       {
 	 Token const1(Symbol::const1, hashval, s);
 	 htable.insert (htable.begin()+hashval, const1);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "array")
       {
 	 Token array(Symbol::array, hashval, s);
 	 htable.insert (htable.begin()+hashval, array);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "integer")
       {
 	 Token integer(Symbol::integer, hashval, s);
 	 htable.insert (htable.begin()+hashval, integer);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "Boolean")
       {
 	 Token Boolean(Symbol::Boolean, hashval, s);
 	 htable.insert (htable.begin()+hashval, Boolean);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "proc")
       {
 	 Token proc(Symbol::proc, hashval, s);
 	 htable.insert (htable.begin()+hashval, proc);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "skip")
       {
 	 Token skip(Symbol::skip, hashval, s);
 	 htable.insert (htable.begin()+hashval, skip);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "read")
       {
 	 Token read(Symbol::read, hashval, s);
 	 htable.insert (htable.begin()+hashval, read);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "write")
       {
 	 Token write(Symbol::write, hashval, s);
 	 htable.insert (htable.begin()+hashval, write);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "call")
       {
 	 Token call(Symbol::call, hashval, s);
 	 htable.insert (htable.begin()+hashval, call);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "if")
       {
 	 Token if1(Symbol::if1, hashval, s);
 	 htable.insert (htable.begin()+hashval, if1);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "do")
       {
 	 Token do1(Symbol::do1, hashval, s);
 	 htable.insert (htable.begin()+hashval, do1);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "fi")
       {
 	 Token fi(Symbol::fi, hashval, s);
 	 htable.insert (htable.begin()+hashval, fi);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "od")
       {
 	 Token od(Symbol::od, hashval, s);
 	 htable.insert (htable.begin()+hashval, od);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "false")
       {
 	 Token false1(Symbol::false1, hashval, s);
 	 htable.insert (htable.begin()+hashval, false1);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else if(s == "true")
       {
 	 Token true1(Symbol::true1, hashval, s);
 	 htable.insert (htable.begin()+hashval, true1);
 	 occupied++;
-	 return hashval+1;
+	 return hashval;
       }
       else//if the reserved word is already in the table...
       {
