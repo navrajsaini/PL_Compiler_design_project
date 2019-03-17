b3c08d5de109b71bc43049e897d994a9ffa70a1b
Navraj Saini <saini4@36aquaman.cs.uleth.ca>
2019-03-17 12:08:32 -0600

    added the .cpp file for blocktable

---
+++ blocktable.cpp
@@ -0,0 +1,32 @@
+#include "blocktable.h"
+#include "administration.h"
+
+BlockTable::BlockTable()
+{
+
+}
+
+bool BlockTable::search(int index)
+{
+   
+}
+
+bool BlockTable::define (int index, Kind nkind, myType ntype, int nsize, int nvalue)
+{
+
+}
+
+TableEntry BlockTable::find (int index, bool &error)
+{
+
+}
+
+bool BlockTable::newBlock()
+{
+   
+}
+
+void BlockTable::endBlock()
+{
+   
+}
